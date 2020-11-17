95f5e818ce7345e87fdbc75ba2e84ce4fb6230cc
techtwins <gshevaughn@gmail.com>
2020-11-17 17:17:17 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+let dodger = document.querySelector('#dodger')
+dodger.style.backgroundColor = 'red'
+const moveDodgerLeft = () => {
+  let leftNumbers = dodger.style.left.replace("px", "")
+  let left = parseInt(leftNumbers, 10)
+
+  if (left > 0) {
+    dodger.style.left = `${left - 15}px`
+  }
+}
+
+document.addEventListener("keydown", e => {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft()
+  }
+})
+
+/* Move Right */
+
+const moveDodgerRight = () => {
+  let leftNumbers = dodger.style.left.replace('px', '')
+  let left = parseInt(leftNumbers, 10)
+
+  if (left < 360) {
+    dodger.style.left = `${left + 15}px`
+  }
+}
+document.addEventListener("keydown", e => {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight()
+  }
+})
